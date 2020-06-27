a81dcc368354f16c7ee16da0c2184a98edfa21cf
Haifa <haifa.haddad@tum.de>
2020-06-27 12:08:03 +0100

    add question controller

---
+++ backend/src/controllers/question.js
@@ -0,0 +1,100 @@
+"use strict";
+
+const QuestionModel = require("../models/question");
+
+const create = async (req, res) => {
+  if (Object.keys(req.body).length === 0)
+    return res.status(400).json({
+      error: "Bad Request",
+      message: "The request body is empty",
+    });
+
+  try {
+    let question = await QuestionModel.create(req.body);
+
+    return res.status(201).json(question);
+  } catch (err) {
+    return res.status(500).json({
+      error: "Internal server error",
+      message: err.message,
+    });
+  }
+};
+
+const read = async (req, res) => {
+  try {
+    let question = await QuestionModel.findById(req.params.id).exec();
+
+    if (!question)
+      return res.status(404).json({
+        error: "Not Found",
+        message: `Question not found`,
+      });
+
+    return res.status(200).json(question);
+  } catch (err) {
+    return res.status(500).json({
+      error: "Internal Server Error",
+      message: err.message,
+    });
+  }
+};
+
+const update = async (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).json({
+      error: "Bad Request",
+      message: "The request body is empty",
+    });
+  }
+
+  try {
+    let question = await QuestionModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    }).exec();
+
+    return res.status(200).json(question);
+  } catch (err) {
+    return res.status(500).json({
+      error: "Internal server error",
+      message: err.message,
+    });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    await QuestionModel.findByIdAndRemove(req.params.id).exec();
+
+    return res
+      .status(200)
+      .json({ message: `Question with id${req.params.id} was deleted` });
+  } catch (err) {
+    return res.status(500).json({
+      error: "Internal server error",
+      message: err.message,
+    });
+  }
+};
+
+const list = async (req, res) => {
+  try {
+    let questions = await QuestionModel.find({}).exec();
+
+    return res.status(200).json(questions);
+  } catch (err) {
+    return res.status(500).json({
+      error: "Internal server error",
+      message: err.message,
+    });
+  }
+};
+
+module.exports = {
+  create,
+  read,
+  update,
+  remove,
+  list,
+};
