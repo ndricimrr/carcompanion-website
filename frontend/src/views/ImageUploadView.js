611c1b4feea00cd84e82da821814dc9682a86382
Haifa Haddad <38734230+HaifaHa@users.noreply.github.com>
2020-06-27 10:08:30 +0100

    Update frontend/src/views/ImageUploadView.js
    Co-authored-by: Ndricim Rrapi <ndricimrr@gmail.com>

--- frontend/src/views/ImageUploadView.js
+++ frontend/src/views/ImageUploadView.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { DropzoneDialog } from "material-ui-dropzone";
+import Button from "@material-ui/core/Button";
 export class ImageUploadView extends React.Component {
    constructor(props) {
     super(props);
