c3f9815095030fa23ff5de885e6c0e4f84508cdc
Haifa <haifa.haddad@tum.de>
2020-06-30 20:01:20 +0100

    create homepage welcome header

--- frontend/src/views/HomePage.js
+++ frontend/src/views/HomePage.js
@@ -1,21 +1,52 @@
 import React, { Component } from "react";
 import Page from "../components/Page";
+import styles from "./HomePage.css";
+// import { Grid } from "@material-ui/core";
+import { Grid, Cell } from "react-mdl"
+import logo from "../assets/logo.png";
+
 
 export class HomePage extends Component {
   render() {
     return (
       <Page>
-        <h1
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: "200px",
-          }}
-        >
-          Welcome to Car Companion HomePage
-        </h1>
+      <div style={{width: "100%", margin: "auto"}}>
+        <Grid className={styles.landingGrid}>
+          <Cell col={12} > </Cell>
+            {/* <img width={100} src={logo} alt="logo" /> */}
+            <div >
+              <br/>
+              <br/>
+              <hr className={styles.bannerFirstLine}></hr>
+              <br/>
+              <h1 className={styles.bannerTitle}>Welcome to Car Companion</h1>
+              <h1 className={styles.bannerSubtitle}> A new way of car ownership.</h1>
+              <hr className={styles.bannerSecondLine}></hr>
+            </div>
+        </Grid>
+      </div>
       </Page>
+
+      // <Page>
+      //    {/* Welcome Header */}
+      //    <header class="masthead">
+      //        <div className={styles.container}>
+      //            <div class="masthead-subheading">Welcome To Our Studio!</div>
+      //            <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
+      //            <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+      //        </div>
+      //    </header>
+      //    <h1
+      //     style={{
+      //       display: "flex",
+      //       justifyContent: "center",
+      //       alignItems: "center",
+      //       marginTop: "200px",
+      //     }}
+      //   >
+      //     Welcome to Car Companion HomePage
+      //   </h1>
+      // </Page>
     );
   }
 }
