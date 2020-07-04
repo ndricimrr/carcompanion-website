e40c74f87b35af0f0f60fbeb402708cbd57add69
ndricim.rrapi <ndricim.rrapi@tum.de>
2020-07-04 14:09:00 +0200

    connected endpoint with find-car front-end tab

--- frontend/src/views/FindCarGridView.js
+++ frontend/src/views/FindCarGridView.js
@@ -3,126 +3,54 @@
  */
 
 // Imports
-import React from "react";
+import React, { Component } from "react";
 import CarBox from "../components/CarBox";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
+import CarService from "../services/CarService";
 
-// Component styles
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-}));
+class FindCarGridView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      cars: [],
+    };
+  }
 
-const FindCarGridView = (props) => {
-  const classes = useStyles();
+  componentWillMount() {
+    this.setState({
+      loading: true,
+    });
 
-  // dummy data for freelancers !!!! to be taken from database
-  var cars = [
-    {
-      image:
-        "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      model: "TOP RATED",
-      make: "JOHN SMITH",
-      year: "MECHANIC",
-      mileage: "328",
-      price: "5",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      model: "TOP RATED",
-      make: "JOHN SMITH",
-      year: "MECHANIC",
-      mileage: "328",
-      price: "5",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      model: "TOP RATED",
-      make: "JOHN SMITH",
-      year: "MECHANIC",
-      mileage: "328",
-      price: "5",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      model: "TOP RATED",
-      make: "JOHN SMITH",
-      year: "MECHANIC",
-      mileage: "328",
-      price: "5",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      model: "TOP RATED",
-      make: "JOHN SMITH",
-      year: "MECHANIC",
-      mileage: "328",
-      price: "5",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      model: "TOP RATED",
-      make: "JOHN SMITH",
-      year: "MECHANIC",
-      mileage: "328",
-      price: "5",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      model: "TOP RATED",
-      make: "JOHN SMITH",
-      year: "MECHANIC",
-      mileage: "328",
-      price: "5",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      model: "TOP RATED",
-      make: "JOHN SMITH",
-      year: "MECHANIC",
-      mileage: "328",
-      price: "5",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      model: "TOP RATED",
-      make: "JOHN SMITH",
-      year: "MECHANIC",
-      mileage: "328",
-      price: "5",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      model: "TOP RATED",
-      make: "JOHN SMITH",
-      year: "MECHANIC",
-      mileage: "328",
-      price: "5",
-    },
-  ];
+    CarService.getCars()
+      .then((cars) => {
+        this.setState({
+          cars: [...cars],
+          loading: false,
+        });
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }
 
-  return (
-    <React.Fragment>
-      <Grid container justify="center" spacing={4}>
-        {cars.map((car, index) => (
-          <Grid key={index} item>
-            <CarBox key={index} car={car} />
-          </Grid>
-        ))}
-      </Grid>
-    </React.Fragment>
-  );
-};
+  render() {
+    if (this.state.loading) {
+      return <h2>Loading Cars...</h2>;
+    }
+    return (
+      <React.Fragment>
+        <Grid container justify="center" spacing={4}>
+          {this.state.cars.map((car, index) => (
+            <Grid key={index} item>
+              <CarBox key={index} car={car} />
+            </Grid>
+          ))}
+        </Grid>
+      </React.Fragment>
+    );
+  }
+}
 
 export default FindCarGridView;
