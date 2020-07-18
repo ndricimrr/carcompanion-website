3475c2ad9911abcfa107482ee6f3f8769c4b444a
ndricim.rrapi <ndricim.rrapi@tum.de>
2020-07-18 11:50:28 +0200

    update buttons

--- frontend/src/views/RequestView/RequestListView.js
+++ frontend/src/views/RequestView/RequestListView.js
@@ -8,7 +8,14 @@ import DriveEtaIcon from "@material-ui/icons/DriveEta";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 import RequestService from "../../services/RequestService";
 import UserService from "../../services/UserService";
-
+import Accordion from "@material-ui/core/Accordion";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import CheckIcon from "@material-ui/icons/Check";
+import CloseIcon from "@material-ui/icons/Close";
+import Button from "@material-ui/core/Button";
 class RequestListView extends Component {
   constructor(props) {
     super(props);
@@ -43,22 +50,46 @@ class RequestListView extends Component {
       <List component="nav" aria-label="main mailbox folders">
         {items.map((item) => {
           return (
-            <ListItem
-              button
-              key={item._id}
-              onClick={() => {
-                this.props.history.push(`/requests/${item._id}`);
-              }}
-            >
-              <ListItemIcon>
-                {item.type === "purchase" ? (
-                  <DriveEtaIcon />
-                ) : (
-                  <SupervisorAccountIcon />
-                )}
-              </ListItemIcon>
-              <ListItemText primary={item.message.concat(" " + item._id)} />
-            </ListItem>
+            // <ListItem
+            //   button
+            //   key={item._id}
+            //   // onClick={() => {
+            //   //   this.props.history.push(`/requests/${item._id}`);
+            //   // }}
+            // >
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <ListItemIcon>
+                  {item.type === "purchase" ? (
+                    <DriveEtaIcon />
+                  ) : (
+                    <SupervisorAccountIcon />
+                  )}
+                </ListItemIcon>
+                <ListItemText primary={item.message.concat(" " + item._id)} />
+                <ListItemIcon>
+                  {item.type === "purchase" ? (
+                    <DriveEtaIcon />
+                  ) : (
+                    <CheckCircleIcon style={{ color: "green" }} />
+                  )}
+                </ListItemIcon>
+              </AccordionSummary>
+              <AccordionDetails>
+                User has sent you a request. Accept
+                <CheckIcon
+                  onClick={() => {
+                    alert("hello");
+                  }}
+                />
+                Decline <CloseIcon />
+              </AccordionDetails>
+            </Accordion>
+            // </ListItem>
           );
         })}
       </List>
