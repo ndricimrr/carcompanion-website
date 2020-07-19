e717fdc322fbd4fceafae0fe28b166adc3b2af7a
Haifa <haifa.haddad@tum.de>
2020-07-19 16:58:01 +0100

    added mock data for freelancers + questions

---
+++ backend/mockdata/freelancersMockData.js
@@ -0,0 +1,135 @@
+let arrayofFreelancers = [
+    {
+    name: "John",
+    surname: "Mckinney",
+    rating: 4,
+    inspections: 32,
+    image: "",
+    badge: "medium",
+    expertise: "BMW, Mercedes",
+    telephone: "+4912349000",
+    address: "Augustensstraße, Munich"
+    },
+    {
+    name: "Chelsie",
+    surname: "Browning",
+    rating: 3,
+    inspections: 2,
+    image: "",
+    badge: "beginner",
+    expertise: "BMW, Mini",
+    telephone: "+491902000",
+    address: "Adalbertstraße, Munich"
+    },
+    {
+    name: "Joni",
+    surname: "Lewis",
+    rating: 5,
+    inspections: 230,
+    image: "",
+    badge: "expert",
+    expertise: "BMW, VW",
+    telephone: "+493802000",
+    address: "Türkenstraße, Munich"
+    },
+    {
+    name: "Chris",
+    surname: "Howell",
+    rating: 5,
+    inspections: 30,
+    image: "",
+    badge: "medium",
+    expertise: "Toyota",
+    telephone: "+493490455",
+    address: "Türkenstraße, Garching"
+    },
+    {
+    name: "Anton",
+    surname: "Jenkins",
+    rating: 3,
+    inspections: 43,
+    image: "",
+    badge: "medium",
+    expertise: "Opel, Fiat",
+    telephone: "+49357902",
+    address: "Freisinger Landstraße, Garching"
+    },
+    {
+    name: "Bob",
+    surname: "Brett",
+    rating: 5,
+    inspections: 103,
+    image: "",
+    badge: "advanced",
+    expertise: "Aston Martin",
+    telephone: "+49357902",
+    address: "MoosacherStraße, Munich"
+    },
+    {
+    name: "Gregor",
+    surname: "Burn",
+    rating: 5,
+    inspections: 82,
+    image: "",
+    badge: "medium",
+    expertise: "Porsche",
+    telephone: "+497820300",
+    address: "Keilerstraße, Dachau"
+    },
+    {
+    name: "Chelsie",
+    surname: "Browning",
+    rating: 3,
+    inspections: 2,
+    image: "",
+    badge: "beginner",
+    expertise: "BMW, Mini",
+    telephone: "+491902000",
+    address: "Adalbertstraße, Munich"
+    },
+    {
+    name: "Olivier",
+    surname: "Drake",
+    rating: 2,
+    inspections: 20,
+    image: "",
+    badge: "beginner",
+    expertise: "BMW, VW",
+    telephone: "+49389000",
+    address: "Dorfstraße, Munich"
+    },
+    {
+    name: "Andre",
+    surname: "Goulding",
+    rating: 5,
+    inspections: 36,
+    image: "",
+    badge: "medium",
+    expertise: "Toyota",
+    telephone: "+49321255",
+    address: "Kirchstraße, Munich"
+    },
+    {
+    name: "Charlie",
+    surname: "Prosser",
+    rating: 2,
+    inspections: 49,
+    image: "",
+    badge: "medium",
+    expertise: "Opel, Toyota",
+    telephone: "+494505902",
+    address: "Beethovenstraße, Munich"
+    },
+    {
+    name: "Alexander",
+    surname: "Michelin",
+    rating: 5,
+    inspections: 103,
+    image: "",
+    badge: "advanced",
+    expertise: "Aston Martin",
+    telephone: "+493447902",
+    address: "Hauptstraße, Munich"
+    },
+
+    ]
---
+++ backend/mockdata/questionsMockData.js
@@ -0,0 +1,80 @@
+let arrayofQuestions = [
+    {
+    title: "High temperature in short drive",
+    areas: "Motor",
+    content: "My honda accord vti-L 2000 model is geting hot in only short drive, about an hour of driving with 30kph, any idea what to check?",
+    date: Date(),
+    images: [],
+    answers: []
+    },
+    {
+    title: "Engine Oil Capacity",
+    areas: "Oil",
+    content: "what is the oil capacity for a 2017 toyota tacoma with a 2.7L engine?",
+    date: Date(),
+    images: [],
+    answers: []
+    },
+    {
+    title: "1995 Jeep Cherokee Won’t Start",
+    areas: "Oil",
+    content: "My Jeep wouldn’t even try to turnover. I tried jumping it but that would work either. When I try to start it there isn’t any noise at all",
+    date: Date(),
+    images: [],
+    answers: []
+    },
+    {
+    title: "2013 328i Nearing 120K, Trade In Or Prepare For Future Repairs?",
+    areas: "General",
+    content: " I have owned the 2013 BMW 328i since 5 yrs. Knowing the high maintenance and repairs cost, should I replace it?",
+    date: Date(),
+    images: [],
+    answers: []
+    },
+    {
+    title: "VW Golf 5 Gt Petrol Losing Power",
+    areas: "Motor",
+    content: "VW GOLF 5 GT TSI engine 1.8L,Losing power and it cannot reach 4000 revs. While driving if i accelerate continuous it chokes and then it drags lowly.",
+    date: Date(),
+    images: [],
+    answers: []
+    },
+    {
+    title: "2010 Chrysler Town And Country Dead",
+    areas: "Battery",
+    content: "MY Chrysler is dead, do you think I should replace the battery?",
+    date: Date(),
+    images: [],
+    answers: []
+    },
+    {
+    title: "A Corvette Looking Vehicle",
+    areas: "General",
+    content: "I can’t remember the name of a vehicle that used to look like a 2/3 size of a 1977 Corvette. Does anyone have the answer?",
+    date: Date(),
+    images: [],
+    answers: [ {authorId: "",
+            content: "I think it might be the Opel GT",
+            date: Date() },
+            {authorId: "",
+            content: "Is the the Fiat Barchetta 1997?",
+            date: Date() }
+        ]
+    },
+    {
+    title: "Turn Signals Won’t Work While Driving",
+    areas: "Signals",
+    content: "My 1980 F150 Ranger has issue with the turn signals. They work perfectly before you drive, they work when you put it in gear, but won’t work once the vehicle has started driving.",
+    date: Date(),
+    images: [],
+    answers: []
+    },
+    {
+    title: "Noise Only When In Gear?",
+    areas: "Motor",
+    content: "The car makes a rattling noise from the front end more words the passengers side of the the car it the eco engine what could this be?",
+    date: Date(),
+    images: [],
+    answers: []
+    }
+    ]
