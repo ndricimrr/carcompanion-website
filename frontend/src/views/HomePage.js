bb60223570ec6a3795873637fc24754aa4df8f26
Haifa Haddad <38734230+HaifaHa@users.noreply.github.com>
2020-07-19 12:51:23 +0100

    homepage UI responsive (#106)
    * made homepage UI responsive
    
    * Fixed web responsiveness for homepage
    
    Co-authored-by: Argert Boja <argertboja@gmail.com>

--- frontend/src/views/HomePage.js
+++ frontend/src/views/HomePage.js
@@ -1,148 +1,69 @@
 import React, { Component } from "react";
 import Page from "../components/Page";
 import styles from "./HomePage.css";
-// import { Grid } from "@material-ui/core";
-import { Grid, Cell } from "react-mdl";
+import Grid from "@material-ui/core/Grid";
 import findDreamCar from "../assets/findDreamCar.jpg";
 import freeInspection from "../assets/freeInspection.jpg";
 import cheapRepair from "../assets/CheapRepair.jpg";
 import carCommunity from "../assets/carCommunity.jpg";
+import { Card, CardMedia, CardContent } from "@material-ui/core";
+import Typography from '@material-ui/core/Typography';
+import GridList from '@material-ui/core/GridList';
+
+
 
 export class HomePage extends Component {
   render() {
+    let services = [
+      [findDreamCar, "Find your dream car", "A user friendly and intelligent search engine for the car that best suits your needs"],
+      [freeInspection, "Free expert inspection", "We match you with experts that help you with inspection when buying your dream car"],
+      [cheapRepair, "Convenient car maintenance & repair", "We match you with freelance mechanics in your neighborhood for car maintenance and repair"],
+      [carCommunity, "Be part of a great community", "A community that relies car experts, enthusiasts and owners - the Stackoverflow for cars."],
+    ]
     return (
       <Page>
         <header className={styles.masthead}>
           <div className={styles.container}>
             <div className={styles.mastheadHeading}>
-              {" "}
               Welcome to car companion!
             </div>
             <div className={styles.mastheadSubheading}>
-              {" "}
-              A new way of car ownership{" "}
+            We accompany you during your whole journey of car ownership
             </div>
           </div>
         </header>
 
-        <section style={{ padding: "6rem 0" }} id="services">
-          <div className={styles.container}>
-            <div className={styles.textCenter}>
-              <h2 className={styles.titleH2}>Services</h2>
-              <h3 className={styles.titleH3}>
-                We accompany you during your whole journey of car ownership
-              </h3>
+        <section style={{ padding: "20px 0" }} id="services">
+            <div style={{display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'space-around',
+                overflow: 'hidden'}}>
+              <Grid container justify="center" spacing={4} className={styles.GridContainer}>
+                {
+                  services.map( (item) => (
+                    <Grid key={item[1]} item className={styles.GridItem}>
+                      <Card style={{height:'200', marginTop:'30px'}}>
+                        <CardMedia
+                        component="img"
+                        height="200"
+                        image={item[0]}
+                        />
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            {item[1]}
+                          </Typography>
+                          <Typography variant="body" color="textSecondary" component="p">
+                            {item[2]}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>)
+                  )
+                }
+              </Grid>
             </div>
-            <div className={styles.rowCenterText}>
-              <div className={styles.column}>
-                <img className={styles.imgFormat} src={findDreamCar} />
-                <div className={styles.centeredTextOnImg}>
-                  1. Find your dream car
-                </div>
-                <div className={styles.smallTextOnImg}>
-                  A user friendly and intelligent search engine for the car that
-                  best suits your needs
-                </div>
-              </div>
-              <div className={styles.column}>
-                <img className={styles.imgFormat} src={freeInspection} />
-                <div className={styles.centeredTextOnImg}>
-                  2. Get free expert inspection
-                </div>
-                <div className={styles.smallTextOnImg}>
-                  We match you with experts that help you with inspection when
-                  buying your dream car
-                </div>
-              </div>
-              <div className={styles.column}>
-                <img className={styles.imgFormat} src={cheapRepair} />
-                <div className={styles.centeredTextOnImg}>
-                  3. Convenient maintenance & repair
-                </div>
-                <div className={styles.smallTextOnImg}>
-                  We match you with freelance mechanics in your neighborhood for
-                  car maintenance and repair
-                </div>
-              </div>
-              <div className={styles.column}>
-                <img className={styles.imgFormat} src={carCommunity} />
-                <div className={styles.centeredTextOnImg}>
-                  4. Be part of a great community
-                </div>
-                <div className={styles.smallTextOnImg}>
-                  A community that relies car experts, enthusiasts and owners -
-                  the Stackoverflow for cars.
-                </div>
-              </div>
-            </div>
-          </div>
         </section>
       </Page>
-
-      // <Page>
-      //    {/* Welcome Header */}
-      //    <header class="masthead">
-      //        <div className={styles.container}>
-      //            <div class="masthead-subheading">Welcome To Our Studio!</div>
-      //            <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
-      //            <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
-      //        </div>
-      //    </header>
-      //    <h1
-      //     style={{
-      //       display: "flex",
-      //       justifyContent: "center",
-      //       alignItems: "center",
-      //       marginTop: "200px",
-      //     }}
-      //   >
-      //     Welcome to Car Companion HomePage
-      //   </h1>
-      // </Page>
-      //2nd versuch:
-      //<div style={{width: "100%", margin: "auto"}}>
-      //     <Grid className={styles.landingGrid}>
-      //     <Cell col={12} > </Cell>
-      //       {/* <img width={100} src={logo} alt="logo" /> */}
-      //       <div>
-      //         <br/>
-      //         <br/>
-      //         <br/>
-      //         <hr className={styles.bannerFirstLine}></hr>
-      //         <br/>
-      //         <h1 className={styles.bannerTitle}>Welcome to Car Companion</h1>
-      //         <h1 className={styles.bannerSubtitle}> A new way of car ownership.</h1>
-      //         <hr className={styles.bannerSecondLine}></hr>
-      //         <br/>
-      //         <br/>
-      //         <br/>
-      //         <br/>
-      //         <br/>
-      //       </div>
-
-      //   <div className={styles.container}>
-      //     <div style={{textAlign: "center"}}>
-      //       <h2 className={styles.titleH2}>Your benefits</h2>
-      //       <h3 className={styles.titleH3}>We bundle all the services around your car and accompany you during the whole journey of car ownership.</h3>
-      //     </div>
-      //     <div className={styles.row}>
-      //       <div className={styles.column}>
-      //         <div className={styles.benefit}>
-      //           <img className={styles.benefitImg} src={findCar} alt="findCar"/>
-      //           <h4>Find your dream car</h4>
-      //           <p style={{color: "#6c757d"}}>User friendly and intelligent search engine to find the best car that suits your needs.</p>
-      //         </div>
-      //         <div className={styles.benefit}>
-      //           <img className={styles.benefitImg} src={inspection} alt="inspection"/>
-      //           <h4>Find your dream car</h4>
-      //           <p style={{color: "#6c757d"}}>User friendly and intelligent search engine to find the best car that suits your needs.</p>
-      //         </div>
-      //       </div>
-
-      //     </div>
-      //   </div>
-      //   </Grid>
-      // </div>
     );
   }
 }
